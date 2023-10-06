760e4779f0291fe283cb23cdb7c531cc70fb13df
emilyjspencer <emilyspencer89@hotmail.co.uk>
2023-10-06 10:51:14 +0100

    chore(Dependencies): Generates new project with dependencies. Changes version of Spring

---
+++ src/main/java/com/example/securityexperiment/SecurityexperimentApplication.java
@@ -0,0 +1,13 @@
+package com.example.securityexperiment;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SecurityexperimentApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SecurityexperimentApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/securityexperiment/SecurityexperimentApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.securityexperiment;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SecurityexperimentApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
